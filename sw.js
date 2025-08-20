a076b40217e687824ce6228c005b1179bd8f025e
rasko65 <radosav.djuric@outlook.com>
2025-08-20 21:41:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,37 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('bunari-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png',
-        'https://cdn.jsdelivr.net/npm/chart.js'
-      ]);
-    })
+const CACHE_NAME = 'bunari-cache-v2';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request).catch(() =>
+        caches.match('./index.html')
+      )
+    )
   );
 });
